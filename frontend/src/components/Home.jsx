bdbdc31acf6ba7321d67c7bffb8059d86149703c
Charmagne Cayetano <clacayetano@gmail.com>
2019-11-17 14:16:35 -0500

    Added missing return in render method of Home component

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,23 +1,16 @@
 // Package imports
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {
-  HashRouter, Route, Switch, Redirect,
-} from 'react-router-dom';
 
 // Local imports
 import { actions } from '../store';
 
-class _Home extends Component {
+class Home extends Component {
   render() {
-      <div> Home </div>
+    return <div> Home </div>;
   }
 }
 
 const mapStateToProps = ({}) => ({});
 
-const Home = connect(
-  mapStateToProps,
-)(_Home);
-
-export default Home;
+export default connect(mapStateToProps)(Home);
