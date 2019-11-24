d249e1c2468b825a08fbcf914a3a5d10cc3e1ecd
Stella Kim <testcodesk@gmail.com>
2019-11-23 20:11:25 -0500

    facebook get user info & add instagram auth to connect

---
+++ backend/routes/auth/instagram.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const dotenv = require("dotenv").config();
+
+
+/* instagram authorization */
+router.get("/", (req, res, next) => {
+    const URL = `https://api.instagram.com/oauth/authorize
+    ?app_id=${process.env.INSTAGRAM_APP_ID}
+    &redirect_uri=${IG_REDIRECT_URI}
+    &scope=user_profile,user_media
+    &response_type=code`;
+});
+
+module.exports = router;
