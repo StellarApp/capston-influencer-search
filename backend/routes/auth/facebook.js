a17d5162b89cd3ac55e2ded83b21ef2491448add
Charmagne Cayetano <clacayetano@gmail.com>
2019-11-30 01:04:00 -0500

    Use Facebook ID as basis for creating new Creator records

--- backend/routes/auth/facebook.js
+++ backend/routes/auth/facebook.js
@@ -4,7 +4,7 @@ const { Creator } = require("../../data/models");
 router.post("/", (req, res, next) => {
   const user = req.body;
   if (user) {
-    Creator.findOrCreate({ where: { email: user.email }, defaults: user })
+    Creator.findOrCreate({ where: { facebookId: user.facebookId }, defaults: user })
       .then(newUser => {
         res.send(newUser.data);
       })
