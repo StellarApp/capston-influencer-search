1c02bffd71afec238403969b4d1597417f220b6f
Charmagne Cayetano <clacayetano@gmail.com>
2019-11-30 00:51:33 -0500

    Do not use https createServer in production

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,25 @@
 require("dotenv").config();
+const fs = require("fs");
+const https = require("https");
 
 const db = require("./data");
 const app = require("./app");
 const port = process.env.PORT || 3000;
-const fs = require("fs");
-const https = require("https");
+
+const startUpCallback = () => console.log(`listening on port ${port}`);
 
 db.sync(true).then(() => {
-  // enable https in Express
-  https
-    .createServer(
-      {
-        key: fs.readFileSync("server.key"),
-        cert: fs.readFileSync("server.cert")
-      },
-      app
-    )
-    .listen(port, function() {
-      console.log(`listening on port ${port}`);
-    });
+  if (process.env.NODE_ENV === 'production') {
+    app.listen(port, startUpCallback);
+  } else {
+    https
+      .createServer(
+        {
+          key: fs.readFileSync("server.key"),
+          cert: fs.readFileSync("server.cert")
+        },
+        app
+      )
+      .listen(port, startUpCallback);
+  }
 });
