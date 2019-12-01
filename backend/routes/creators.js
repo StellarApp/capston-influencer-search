54683dd150f3460add53ded91391ef7642c6e1c6
Charmagne Cayetano <clacayetano@gmail.com>
2019-12-01 11:55:51 -0500

    Define express to remove ReferenceError

--- backend/routes/creators.js
+++ backend/routes/creators.js
@@ -1,4 +1,5 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
 const { Creator } = require('../data').models;
 
 router.use(express.json());
