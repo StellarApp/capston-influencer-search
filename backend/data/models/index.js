f8c1fdd608561e9ac1acad9aadf4acdcd640b0d0
Charmagne Cayetano <clacayetano@gmail.com>
2019-12-03 00:26:29 -0500

    Added Keyword model

--- backend/data/models/index.js
+++ backend/data/models/index.js
@@ -1,11 +1,14 @@
-const Creator = require("./Creator");
-const Business = require("./Business");
-const CreatorInsight = require("./CreatorInsight");
+const Creator = require('./Creator');
+const Business = require('./Business');
+const CreatorInsight = require('./CreatorInsight');
+const Keyword = require('./Keyword');
+
 Creator.hasMany(CreatorInsight);
 CreatorInsight.belongsTo(Creator);
 
 module.exports = {
   Creator,
   Business,
-  CreatorInsight
+  CreatorInsight,
+  Keyword
 };
