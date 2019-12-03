23caec7570b61636c4b9464967287be89d2193be
Charmagne Cayetano <clacayetano@gmail.com>
2019-12-03 00:55:23 -0500

    Seed database with keywords

--- backend/data/sync.js
+++ backend/data/sync.js
@@ -1,6 +1,12 @@
 const connection = require('./connection');
+const seed = require('./seed');
+
 const sync = async (force = false) => {
   await connection.sync({ force });
+
+  if (force) {
+    await seed();
+  }
 };
 
 module.exports = sync;
