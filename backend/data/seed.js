501745e1667e26b626e8318184b78c66043bc8e6
Charmagne Cayetano <clacayetano@gmail.com>
2019-12-03 00:55:23 -0500

    Seed database with keywords

---
+++ backend/data/seed.js
@@ -0,0 +1,46 @@
+const { Keyword } = require('./models');
+
+const keywordList = [
+  'Adventure',
+  'Art',
+  'Baby',
+  'Beauty',
+  'Business',
+  'Craft',
+  'Decorating',
+  'Design',
+  'DIY',
+  'Education',
+  'Entertainment',
+  'Fashion',
+  'Film',
+  'Finance',
+  'Fitness',
+  'Food',
+  'Gaming',
+  'Health',
+  'Home Decor',
+  'Humor',
+  'Lifestyle',
+  'Makeup',
+  'Marketing',
+  'Mom',
+  'Music',
+  'Outdoor',
+  'Parenting',
+  'Pet',
+  'Photography',
+  'Political',
+  'Relationships',
+  'Self-help',
+  'Sewing',
+  'Sports',
+  'Tech',
+  'Travel',
+  'Wedding'
+];
+
+const seed = () =>
+  Promise.all(keywordList.map(keyword => Keyword.create({ name: keyword })));
+
+module.exports = seed;
