42dfb43020ac5d76f5b4f589cec36ed16a8ae8fc
Charmagne Cayetano <clacayetano@gmail.com>
2019-12-04 01:30:53 -0500

    Use async/await in syncInsight to await CreatorInsight creation

--- backend/utilites/syncInsight.js
+++ backend/utilites/syncInsight.js
@@ -1,58 +1,45 @@
-require("dotenv").config();
-const axios = require("axios");
-// import { connect } from "react-redux";
-const { CreatorInsight } = require("../data/models/");
+const axios = require('axios');
+
+const { CreatorInsight } = require('../data').models;
 const app_version = process.env.FB_APP_VERSION;
-const api = "https://graph.facebook.com";
+const api = process.env.FB_API;
 
-const syncUserInsight = (instagramId, creatorId, accessToken) => {
-  // Make a request for a user with a given ID
+const syncUserInsight = async (instagramId, creatorId, accessToken) => {
   const fields = [
-    "followers_count",
-    "follows_count",
-    "media_count",
-    "name",
-    "profile_picture_url",
-    "username",
-    "website",
-    "biography"
+    'followers_count',
+    'follows_count',
+    'media_count',
+    'name',
+    'profile_picture_url',
+    'username',
+    'website',
+    'biography'
   ];
+
   const link = `${api}/${app_version}/${instagramId}?fields=${fields.join(
-    ","
+    ','
   )}&access_token=${accessToken}`;
-  axios
-    .get(link)
-    .then(function(response) {
-      // handle success
-      const {
-        followers_count,
-        follows_count,
-        media_count,
-        profile_picture_url,
-        username,
-        biography
-      } = response.data;
 
-      const insight = {
-        igName: username,
-        biography,
-        profilePictureUrl: profile_picture_url,
-        followersCount: followers_count,
-        followsCount: follows_count,
-        mediaCount: media_count,
-        creatorId
-      };
-      CreatorInsight.create(insight);
-    })
-    .catch(function(error) {
-      // handle error
-      console.log(error);
-    })
-    .finally(function() {
-      // always executed
-    });
+  const {
+    followers_count,
+    follows_count,
+    media_count,
+    profile_picture_url,
+    username,
+    biography
+  } = (await axios.get(link)).data;
+
+  const insight = {
+    igName: username,
+    biography,
+    profilePictureUrl: profile_picture_url,
+    followersCount: followers_count,
+    followsCount: follows_count,
+    mediaCount: media_count,
+    creatorId
+  };
+
+  await CreatorInsight.create(insight);
 };
-
-const syncUserMedia = (instagramId, accessToken) => {};
 
 module.exports = { syncUserInsight };
