e71ab5b14ba9600e05b7bc28e6218e2b9ab9217f
Charmagne Cayetano <clacayetano@gmail.com>
2019-12-04 00:19:09 -0500

    Include creator isNew value to backend fb auth response

--- backend/routes/auth/facebook.js
+++ backend/routes/auth/facebook.js
@@ -1,15 +1,19 @@
-const router = require("express").Router();
-const { Creator } = require("../../data/models");
-const { syncUserInsight } = require("../../utilites/syncInsight");
+const router = require('express').Router();
+const { Creator } = require('../../data').models;
+const { syncUserInsight } = require('../../utilites/syncInsight');
 
-router.post("/", (req, res, next) => {
+router.post('/', (req, res, next) => {
   const { token, user } = req.body;
   if (user) {
     Creator.authenticate(user)
-      .then(creator => {
-        // get instagram insight
-        const { instagramId, id } = creator[0];
-        syncUserInsight(instagramId, id, token);
+      .spread((creator, isNewCreator) => {
+        const { instagramId, id } = creator;
+
+        if (isNewCreator) {
+          syncUserInsight(instagramId, id, token);
+        }
+        creator.isNew = isNewCreator;
+
         res.send(creator);
       })
       .catch(ex => console.log(ex));
