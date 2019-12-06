662a17357f94b16812c98a8eba461cbd978aa988
Effy Zhang <effy@effy-0qlvch.local>
2019-12-05 20:37:24 -0500

    add fallback option

--- frontend/src/components/Creator.jsx
+++ frontend/src/components/Creator.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Creator = ({ creators, match: { params } }) => {
+  if ((creators.length = 0)) {
+    return <h1>No Creator Found</h1>;
+  }
   const creator = creators.find(_creator => _creator.id === params.id);
   // const {
   //   creatorId,
