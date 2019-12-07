705711aa8e96b6a165d082e8669a288b524c2fbb
Charmagne Cayetano <clacayetano@gmail.com>
2019-12-07 15:55:46 -0500

    Add POST /api/creators/:id/links route

--- backend/data/models/index.js
+++ backend/data/models/index.js
@@ -4,6 +4,7 @@ const CreatorInsight = require('./CreatorInsight');
 const Keyword = require('./Keyword');
 const Collection = require('./Collection');
 const Interest = require('./Interest');
+const Link = require('./Link');
 
 // Relationships
 Creator.hasMany(CreatorInsight);
@@ -18,11 +19,14 @@ Collection.belongsTo(Creator);
 Creator.hasMany(Interest);
 Interest.belongsTo(Keyword);
 
+Creator.hasMany(Link);
+
 module.exports = {
   Business,
   Collection,
   Creator,
   CreatorInsight,
   Keyword,
-  Interest
+  Interest,
+  Link
 };
