f590111a67a7da79ab92027812c713ee5043cb79
Effy Zhang <effy@effy-0qlvch.local>
2019-12-10 20:21:17 -0500

    update added state

--- frontend/src/components/CreatorCard.jsx
+++ frontend/src/components/CreatorCard.jsx
@@ -65,7 +65,10 @@ const AddBtn = styled(SecondaryButton)`
   width: 3rem;
   background-color: #ffe9e6;
 `;
-
+const AddedBtn = styled(SecondaryButton)`
+  width: 3rem;
+  background-color: #efefef;
+`;
 const EmailBtn = styled(SecondaryButton)`
   width: 3rem;
   margin-right: 0.75rem;
@@ -146,7 +149,23 @@ const CreatorCard = ({
           </svg>
         </EmailBtn>
         {inCollection ? (
-          "Added to your collection"
+          <AddedBtn>
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M20 6L9 17L4 12"
+                stroke="#828282"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </AddedBtn>
         ) : (
           <AddBtn onClick={() => handleAddCollection(businessId, creator.id)}>
             <svg
