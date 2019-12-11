145d1b7d44a279e2e6323baa39f8f8cc038edb0e
Effy Zhang <effy@effy-0qlvch.local>
2019-12-10 21:21:43 -0500

    styled filters

--- frontend/src/components/CreatorSearchBar.jsx
+++ frontend/src/components/CreatorSearchBar.jsx
@@ -4,17 +4,22 @@ import TextField from "@material-ui/core/TextField";
 import FormControl from "@material-ui/core/FormControl";
 import InputLabel from "@material-ui/core/InputLabel";
 import Select from "@material-ui/core/Select";
+// import defaultTheme from "./Theme";
+
 import MenuItem from "@material-ui/core/MenuItem";
 import styled from "styled-components";
 import defaultTheme from "./Theme";
 import { makeStyles } from "@material-ui/core/styles";
+const Container = styled.div`
+  background-color: ${defaultTheme.bg.secondary};
+  padding: 2rem 0;
+`;
 
-const Container = styled.form`
-  display: flex;
-  padding: 3rem;
-  justify-content: space-evenly;
-  width: 100%;
-  border-bottom: 1px solid #bdbdbd;
+const StyledForm = styled.form`
+  display: grid;
+  margin: 0 4rem;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-column-gap: 2rem;
   height: 6rem;
   overflow-y: hidden;
   align-items: center;
@@ -37,45 +42,47 @@ class CreatorSearchBar extends Component {
 
     return (
       <Container>
-        <TextField
-          label="Search by Keywords"
-          variant="outlined"
-          type="search"
-          placeholder="Search by keywords"
-          name="searchText"
-          value={searchText}
-          onChange={handleFilterChange}
-        />
-        <FormControl>
-          <InputLabel>Location</InputLabel>
-          <Select name="searchLocation" onChange={handleFilterChange}>
-            <MenuItem value="">Select All</MenuItem>
-            {locations.map((location, idx) => (
-              <MenuItem key={idx} value={location}>
-                {location}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <FormControl>
-          <InputLabel>Gender</InputLabel>
-          <Select name="searchGender" onChange={handleFilterChange}>
-            <MenuItem value="">Select All</MenuItem>
-            <MenuItem value="female">female</MenuItem>
-            <MenuItem value="male">male</MenuItem>
-          </Select>
-        </FormControl>
-        <FormControl>
-          <InputLabel>Interest</InputLabel>
-          <Select name="searchInterest" onChange={handleFilterChange}>
-            <MenuItem value="">Select All</MenuItem>
-            {keywords.map((interest, idx) => (
-              <MenuItem key={idx} value={interest.id}>
-                {interest.name}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+        <StyledForm>
+          <TextField
+            label="Search by Keywords"
+            variant="outlined"
+            type="search"
+            placeholder="Search by keywords"
+            name="searchText"
+            value={searchText}
+            onChange={handleFilterChange}
+          />
+          <FormControl>
+            <InputLabel>Location</InputLabel>
+            <Select name="searchLocation" onChange={handleFilterChange}>
+              <MenuItem value="">Select All</MenuItem>
+              {locations.map((location, idx) => (
+                <MenuItem key={idx} value={location}>
+                  {location}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl>
+            <InputLabel>Gender</InputLabel>
+            <Select name="searchGender" onChange={handleFilterChange}>
+              <MenuItem value="">Select All</MenuItem>
+              <MenuItem value="female">female</MenuItem>
+              <MenuItem value="male">male</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl>
+            <InputLabel>Interest</InputLabel>
+            <Select name="searchInterest" onChange={handleFilterChange}>
+              <MenuItem value="">Select All</MenuItem>
+              {keywords.map((interest, idx) => (
+                <MenuItem key={idx} value={interest.id}>
+                  {interest.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </StyledForm>
       </Container>
     );
   }
